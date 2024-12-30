b0539cbd689d10cb28b11d4099d59c4e11d76638
Brendon Dong <Brendon4496@gmail.com>
2024-12-29 20:10:16 -0500

    Need to expose headers!

--- p1_bdong_ers/back-end/src/main/java/com/revature/bdong_ers/Controllers/AuthController.java
+++ p1_bdong_ers/back-end/src/main/java/com/revature/bdong_ers/Controllers/AuthController.java
@@ -13,14 +13,18 @@ import com.revature.bdong_ers.Services.UserService;
 import jakarta.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
 public class AuthController {
+
+    public static final String AUTH_HEADER_NAME = "authorization";
 
     private ReimbursementService reimbursementService;
     private UserService userService;
@@ -44,19 +48,27 @@ public class AuthController {
     public ResponseEntity<UserIdDTO> registerUser(@RequestBody User user) {
         User registeredUser = userService.registerAccount(user);
         String token = authService.generateToken(registeredUser);
-        return ResponseEntity.ok().header("Authorization", token).body(new UserIdDTO(registeredUser));
+        HttpHeaders headers = createHttpAuthHeaders(token);
+        return ResponseEntity.ok().headers(headers).body(new UserIdDTO(registeredUser));
     }
 
     @PostMapping(value="/login")
     public ResponseEntity<UserResponseDTO> loginUser(@RequestBody UserLoginDTO userLoginDTO) {
-
         User validUser = userService.loginAccount(userLoginDTO);
-        String token = "";
+        HttpHeaders headers = null;
         UserResponseDTO response = null;
         if (validUser != null) {
-            token = authService.generateToken(validUser);
+            String token = authService.generateToken(validUser);
             response = new UserResponseDTO(validUser);
+            headers = createHttpAuthHeaders(token);
         }
-        return ResponseEntity.ok().header("Authorization", token).body(response);
+        return ResponseEntity.ok().headers(headers).body(response);
+    }
+
+    private HttpHeaders createHttpAuthHeaders(String token) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(AUTH_HEADER_NAME, token);
+        httpHeaders.setAccessControlExposeHeaders(Collections.singletonList(AUTH_HEADER_NAME));
+        return httpHeaders;
     }
 }
